22b05503bb9c97f38af1b64ed2b4b6fa04751636
LotalGhana <114388126+LotalGhana@users.noreply.github.com>
2025-07-16 18:17:37 -0700

    Add files via upload

---
+++ firebase-init.js
@@ -0,0 +1,13 @@
+// firebase-init.js
+// Replace this config with your actual Firebase project config
+const firebaseConfig = {
+apiKey: "AIzaSyCpft5uBpz_vl9RzJb6Ryb4vE4qqyHSJ5c",
+    authDomain: "bbrghana.firebaseapp.com",
+    projectId: "bbrghana",
+    storageBucket: "bbrghana.firebasestorage.app",
+    messagingSenderId: "607583775596",
+    appId: "1:607583775596:web:c8a6877b516ee093c8e149"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
